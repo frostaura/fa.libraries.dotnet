8395d60930db0a0a41c05fbf41fcca036473d72f
Dean Martin <dean.martin@derivco.co.za>
2023-02-06 09:57:12 +0200

    feat(Data): Adding named entity base to the data project.

---
+++ src/Data/Models/EntityFramework/BaseNamedEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace FrostAura.Libraries.Data.Models.EntityFramework
+{
+    /// <summary>
+    /// A base for all entities with auto-generated unique identifiers and names.
+    /// </summary>
+    [DebuggerDisplay("Name: {Name}")]
+    public class BaseNamedEntity : BaseEntity
+    {
+        /// <summary>
+        /// Entity name / short description.
+        /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessage = $"A valid name is required.")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
