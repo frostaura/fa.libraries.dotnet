a763db8b24dd5a360642c93c2d7e350847707a78
Dean Martin <dean.martin@derivco.co.za>
2023-02-06 11:01:14 +0200

    feat(Data): Adding FieldIgnoreAttribute.

---
+++ src/Data/Attributes/FieldIgnoreAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace FrostAura.Libraries.Data.Attributes
+{
+    /// <summary>
+    /// Attribute to mark a field as ignored with respect to the dynamic field form generation.
+    /// </summary>
+    public class FieldIgnoreAttribute : Attribute
+    {
+    }
+}
