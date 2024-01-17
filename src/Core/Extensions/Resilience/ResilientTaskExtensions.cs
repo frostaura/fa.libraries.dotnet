bafcd368b15327af56957a8b7955e40275c07cf6
Dean Martin <dean.martin@derivco.co.za>
2024-01-17 14:33:41 +0200

    feat(Extensions): Adding resilience extensions to the core library.

---
+++ src/Core/Extensions/Resilience/ResilientTaskExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Polly;
+
+namespace FrostAura.Libraries.Core.Extensions.Resilience
+{
+	public static class ResilientTaskExtensions
+	{
+        /// <summary>
+        /// A generic retry policy for all Tasks.
+        /// </summary>
+        /// <param name="originalTask">The original work that should be executed in the retry loop.</param>
+        /// <param name="maxRetryCount">The count of retries to attempt before raising the internal exception.</param>
+        /// <param name="retryExponent">The exponent for exponential backoff.</param>
+        /// <returns>A wrapped task with added resilience.</returns>
+		public static Task AsResilientTask(this Task originalTask, int maxRetryCount = 5, double retryExponent = 1.5)
+        {
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(maxRetryCount, retryCount => TimeSpan.FromSeconds(retryCount * retryExponent))
+                .ExecuteAsync(async () => await originalTask);
+        }
+
+        /// <summary>
+        /// A generic retry policy for all Tasks.
+        /// </summary>
+        /// <typeparam name="T">Type of the task.</typeparam>
+        /// <param name="originalTask">The original work that should be executed in the retry loop.</param>
+        /// <param name="maxRetryCount">The count of retries to attempt before raising the internal exception.</param>
+        /// <param name="retryExponent">The exponent for exponential backoff.</param>
+        /// <returns>A wrapped task with added resilience.</returns>
+		public static Task<T> AsResilientTask<T>(this Task<T> originalTask, int maxRetryCount = 5, double retryExponent = 1.5)
+		{
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(maxRetryCount, retryCount => TimeSpan.FromSeconds(retryCount * retryExponent))
+                .ExecuteAsync(async () => await originalTask);
+        }
+	}
+}
